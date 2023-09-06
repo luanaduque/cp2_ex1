60078d293b29e4c3546506d13295669f24c8b276
luanaduque <luanaduque273@gmail.com>
2023-09-05 22:55:30 -0300

    dados na lista Produtos.jsx

--- cp2_ex1/src/routes/Produtos/index.jsx
+++ cp2_ex1/src/routes/Produtos/index.jsx
@@ -3,6 +3,13 @@ export default function Produtos(){
     return(
         <main>
             <h1>Produtos</h1>
+            {listaProdutos.map(prod =>(
+                <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
     )
 }
